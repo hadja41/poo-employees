4041500bc6bac4ff37bfb3a8204b5a1fcd6c9baa
Hadja41 <hadhawabah@gmail.com>
2024-06-23 22:07:55 +0200

    ajout d'élements

--- src/main/java/fr/afpa/employees/Employee.java
+++ src/main/java/fr/afpa/employees/Employee.java
@@ -1,5 +1,8 @@
 package fr.afpa.employees;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 /*
 Objectif :
 L’objectif de ce TP est de concevoir un programme en console basé sur une approche objet et
@@ -9,31 +12,148 @@ Vous allez écrire une classe représentant les salariés d'une entreprise.
 Vous pourrez vous aider du PDF disponible sur pour obtenir des informations sur la façon de faire.
 */
 
-class Employee
-{
+class Employee {
+	// TODO compléter les attributs comme présenté dans le PDF
 	/**
 	 * Matricule de l'employé
 	 */
 	private String registrationNumber;
-
-	// TODO compléter les attributs comme présenté dans le PDF
+	private String lastName;
+	private String firstName;
+	private double salary;
+	private final int socialRate = 30;
+	private LocalDate birthDate;
 
 	// TODO compléter le constructeur de la classe
-	public Employee(String registrationNumber) {
+	public Employee(String registrationNumber, String lastName, String firstName, double salary, String birthDate) {
+		checkRegistrationNumber(registrationNumber);
 		this.registrationNumber = registrationNumber;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.salary = salary;
+		this.birthDate = LocalDate.parse(birthDate);
 	}
 
-	// TODO implémenter les setters et getters de la classe (permet d'accéder aux attributs privés)
+	// TODO implémenter les setters et getters de la classe (permet d'accéder aux
+	// attributs privés)
+
+	// Récuperation des attributs
 	public String getRegistrationNumber() {
-		return registrationNumber;
+		return this.registrationNumber;
 	}
 
-	public void setRegistrationNumber(String registrationNumber) throws Exception {
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public int getSocialRate() {
+		return socialRate;
+	}
+
+	public LocalDate getbirthDate() {
+		return birthDate;
+	}
+
+	// Modification des attributs
+	public void setRegistrationNumber(String registrationNumber) {
 		this.registrationNumber = registrationNumber;
 	}
 
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = LocalDate.parse(birthDate);
+	}
 	// TODO ajouter les getters et les setters
 
-	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère qui représente un objet de la classe employé
-	// plus d'information sur la méthode "toString()" -> https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+	// TODO implémenter la méthode "toString()" qui renvoie une chaîne de caractère
+	// qui représente un objet de la classe employé
+	// plus d'information sur la méthode "toString()" ->
+	// https://codegym.cc/fr/groups/posts/fr.986.mthode-java-tostring
+
+	// Fonction pour obtenir le nom complet
+	public String toString() {
+		return "Employee {"
+				+ "\nNom: " + getFirstName()
+				+ "\nPrenom: " + getLastName()
+				+ "\nSalaire: " + getSalary()
+				+ "\nDate de naissance: " + getbirthDate()
+				+ "\nSalaire net: " + netSalary()
+				+ "}";
+	}
+
+	// Fonction pour calculer le salaire net
+	public double netSalary() {
+		return this.salary - (this.salary * this.socialRate) / 100.0;
+
+	}
+
+	// Fonction pour calculer le nombre de jours restant avant la date
+	// d'anniversaire
+	public Long daysBeforeBirthDate() {
+
+		//Pour obtenir la date actuelle
+		LocalDate currentDate = LocalDate.now();
+
+		//Pour obtenir la date d'anniversaire de l'année en cours
+		LocalDate nextBirthDate = birthDate.withYear(currentDate.getYear());
+
+		//Boucle si la date de l'anniversaire est dejà passé, prendre celle de l'année prochaine
+		if (nextBirthDate.isBefore(currentDate) || nextBirthDate.isEqual(currentDate)){
+		 	nextBirthDate = nextBirthDate.plusYears(1);
+		}
+	
+		return ChronoUnit.DAYS.between(currentDate, nextBirthDate);
+
+	}
+
+	//Fonction pour verifier si le matricule est correct ou incorrect
+	private boolean checkRegistrationNumber(String stringToCheck){
+		if (stringToCheck == null || stringToCheck.length() !=7){
+			return false;
+		}
+
+		//Vérifier que les deux premiers et les deux derniers caractères sont des chiffres
+		for (int i = 0; i < stringToCheck.length(); i++){
+			if (i == 0 || i == 1 || i == 5 || i == 6){
+				if (!Character.isDigit(stringToCheck.charAt(i))){
+					return false;
+				}
+			}
+		}
+
+		//Vérifier que les caractères sont des lettres 
+		for (int i = 2; i <= 4; i++){
+			if (!Character.isLetter(stringToCheck.charAt(i))){
+				return false;
+			}
+
+		}
+		return true;
+
+	}
+
+		
+
 }
+
+
--- src/main/java/fr/afpa/employees/EmployeeMain.java
+++ src/main/java/fr/afpa/employees/EmployeeMain.java
@@ -6,16 +6,42 @@ import java.time.temporal.ChronoUnit;
 /**
  * Classe principale du projet, contient la fonction "main"
  */
-class EmployeeMain
-{
-	public static void main(String[] args) 
-	{
+class EmployeeMain {
+	public static void main(String[] args) {
 		System.out.println("\n----- Exercice de programmation objet - classe \"Employee\" -----");
 
-		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci implémentée)
-		Employee employee1 = new Employee("11ABC22");
+		// TODO instanciez 4 objets de la classe "Employee" (une fois celle-ci
+		// implémentée)
+		Employee employee1 = new Employee("11ABC22", "Paul", "Thomas", 1400.0, "1999-09-17");
+		Employee employee2 = new Employee("11ABC23", "Toto", "Thomas", 1900.0, "2002-01-23");
+		Employee employee3 = new Employee("11ABC24", "Tata", "Thomas", 2100.0, "2002-06-23");
+		Employee employee4 = new Employee("11ABC25", "Ben", "Thomas", 1700.0, "2005-01-23");
 
+		// Appel de la fonction à partir de l'objet employee1
+		
+		System.out.println("\n------Employe1------");
+		System.out.println(employee1);
+		System.out.println(employee1.toString());
+		System.out.println(employee1.netSalary());
+		System.out.println("Nombre de jours restant avant la date d'anniversaire: " + employee1.daysBeforeBirthDate());
+		
+		System.out.println("\n------Employe2------");
+		System.out.println(employee2);
+		System.out.println(employee2.toString());
+		System.out.println(employee2.netSalary());
+		System.out.println("Nombre de jours restant avant la date d'anniversaire: " + employee2.daysBeforeBirthDate());
+		
+		System.out.println("\n------Employe3------");
+		System.out.println(employee3);
+		System.out.println(employee3.toString());
+		System.out.println(employee3.netSalary());
+		System.out.println("Nombre de jours restant avant la date d'anniversaire: " + employee3.daysBeforeBirthDate());
+		
+		System.out.println("\n------Employe4------");
+		System.out.println(employee4);
+		System.out.println(employee4.toString());
+		System.out.println(employee4.netSalary());
+		System.out.println("Nombre de jours restant avant la date d'anniversaire: " + employee4.daysBeforeBirthDate());
 		// TODO afficher les informations des employés avec System.out.println
 	}
 }
-
